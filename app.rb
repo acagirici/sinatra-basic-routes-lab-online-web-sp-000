4dfbcf2d866deb6db1b75a5ef8b85177ecf612f7
Arif Cagirici <acagirici23@gmail.com>
2020-12-28 04:26:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,5 +1,11 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do 
+    "Hello, World!"
+  end
   
+  get '/name' do
+    @name = Name.all
+    erb :'name/index.html.erb'
 end
